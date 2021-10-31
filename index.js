48ee21efc5fafae1f77ed8ece19d5f2751a070ac
agildw <yudistiraagil24@gmail.com>
2021-10-31 16:50:31 +0700

    add create wallet

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
+const Web3 = require('web3');
+const web3 = new Web3('https://bsc-dataseed1.binance.org:443');
+
 
 //insert your bot token here
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply('Hello can i help you?'))
 
+//create wallet command
+bot.command('createwallet', (ctx) => {
+    const resultWallet = web3.eth.accounts.create();
+    const backTick = '`';
+    ctx.reply(
+        `*Address* : ${backTick}${resultWallet.address}${backTick}\n*Private Key * : ${backTick}${resultWallet.privateKey}${backTick}`, { parse_mode: "MarkdownV2" })
+})
 //start bot
 bot.launch()
 
